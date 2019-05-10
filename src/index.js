52f32d4291b042ec6c0974a2a5d2ec2311431a5e
alena-osipenko <alena-osipenko8@gmail.com>
2019-05-10 15:25:25 +0300

    feat: add starting files
    Add markup
    Add scss folder
    Add main JavaScript file

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import './scss/main.scss';
+import 'bootstrap';
+import 'owl.carousel';
+
+
+console.log('Hello, world!');
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel();
+});
