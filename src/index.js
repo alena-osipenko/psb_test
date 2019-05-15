37911fa81b6ba91832a784597f19fb29fd50bc0b
alena-osipenko <alena-osipenko8@gmail.com>
2019-05-15 22:57:23 +0300

    Make FirstScreen, Header, Steps and FAQ

--- src/index.js
+++ src/index.js
@@ -8,13 +8,50 @@ import 'owl.carousel';
 console.log('Hello, world!');
 
 $(document).ready(function () {
-    // $('.steps').owlCarousel();
-    $('.collapse').collapse({parent: '.karen'});
-    $('.collapse').on('click', (e)=>{
-        console.log(123);
-        $(e.target).collapse({parent: '.karen'});
-    })
+    $('.steps-carousel').owlCarousel({
+        center: true,
+        items:2,
+        loop:false,
+        margin:8,
+        autoWidth: true,
+        responsive:{
+            600: {
+                margin:24,
+                items: 3
+            },
+                1280:{
+                center: false,
+                responsiveClass:true,
+                loop:false,
+                margin:24,
+                items: 4
+            }
+        }
+    });
 
+    $('.fsc-carousel').owlCarousel({
+        margin:16,
+        items: 5,
+        dots: true,
+        loop: true,
+        responsiveClass:true,
+        navText: ['<svg width="12" height="26" viewBox="0 0 12 26" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11 1L2 13L11 25" stroke="#646478" stroke-width="2"></svg>', '<svg width="12" height="26" viewBox="0 0 12 26" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L10 13L1 25" stroke="#646478" stroke-width="2"></svg>'],
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:1,
+                nav:false
+            },
+            1000:{
+                items:1,
+                nav:true,
+                loop:true
+            }
+        }
+    });
 
 
     $(".fs-carousel").length && function () {
